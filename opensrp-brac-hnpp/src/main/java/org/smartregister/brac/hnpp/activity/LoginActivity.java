8dd09ecda3eec6c11b44b1af4f50d80f015a7341
rotnocse@gmail.com <rotnocse@gmail.com>
2020-11-26 10:43:29 +0600

    stock module

--- opensrp-brac-hnpp/src/main/java/org/smartregister/brac/hnpp/activity/LoginActivity.java
+++ opensrp-brac-hnpp/src/main/java/org/smartregister/brac/hnpp/activity/LoginActivity.java
@@ -40,6 +40,7 @@ import org.smartregister.brac.hnpp.HnppApplication;
 import org.smartregister.brac.hnpp.R;
 import org.smartregister.brac.hnpp.job.PullHouseholdIdsServiceJob;
 import org.smartregister.brac.hnpp.job.SSLocationFetchJob;
+import org.smartregister.brac.hnpp.job.StockFetchJob;
 import org.smartregister.brac.hnpp.job.TargetFetchJob;
 import org.smartregister.brac.hnpp.location.SaveDistrictTask;
 import org.smartregister.brac.hnpp.presenter.LoginPresenter;
@@ -379,6 +380,7 @@ public class LoginActivity extends BaseLoginActivity implements BaseLoginContrac
             PullHouseholdIdsServiceJob.scheduleJobImmediately(PullHouseholdIdsServiceJob.TAG);
             SSLocationFetchJob.scheduleJobImmediately(SSLocationFetchJob.TAG);
             TargetFetchJob.scheduleJobImmediately(TargetFetchJob.TAG);
+            StockFetchJob.scheduleJobImmediately(StockFetchJob.TAG);
         }catch (Exception e){
 
         }
