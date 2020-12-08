9dd41de29fd12c9a8f7ecfbc5f5d00ed6f74652b
mahmud6390 <mahmud6390@gmail.com>
2020-12-08 11:55:57 +0600

    PA

--- opensrp-brac-hnpp/src/main/java/org/smartregister/brac/hnpp/activity/SkSelectionActivity.java
+++ opensrp-brac-hnpp/src/main/java/org/smartregister/brac/hnpp/activity/SkSelectionActivity.java
@@ -1,6 +1,8 @@
 package org.smartregister.brac.hnpp.activity;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Spinner;
 
 import org.smartregister.brac.hnpp.R;
 import org.smartregister.view.activity.SecuredActivity;
@@ -8,10 +10,31 @@ import org.smartregister.view.activity.SecuredActivity;
 public class SkSelectionActivity extends SecuredActivity {
 
     private RecyclerView recyclerView;
+    private Spinner skSpinner,ssSpinner;
+
     @Override
     protected void onCreation() {
         setContentView(R.layout.activity_sk_select);
         recyclerView = findViewById(R.id.recycler_view);
+        skSpinner = findViewById(R.id.sk_filter_spinner);
+        ssSpinner = findViewById(R.id.ss_filter_spinner);
+        findViewById(R.id.add_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        findViewById(R.id.update_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+        updateSpinner();
+
+    }
+    private void updateSpinner(){
 
     }
 
