3805e8f80fdcc1044b767d99941c248be09ffab0
mahmud6390 <mahmud6390@gmail.com>
2020-12-17 12:14:35 +0600

    build

--- opensrp-brac-hnpp/src/main/java/org/smartregister/brac/hnpp/model/Migration.java
+++ opensrp-brac-hnpp/src/main/java/org/smartregister/brac/hnpp/model/Migration.java
@@ -1,9 +1,14 @@
 package org.smartregister.brac.hnpp.model;
 
+import org.smartregister.clientandeventmodel.Address;
+
+import java.util.ArrayList;
+
 public class Migration {
     public String baseEntityId;
     public String firstName;
     public String birthdate;
     public String gender;
+    public ArrayList<Address> addresses;
 
 }
