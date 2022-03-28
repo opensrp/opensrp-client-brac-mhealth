ff4dae1f636b12b9ccbe0b55d6ee1201328b5738
mahmud6390 <mahmud6390@gmail.com>
2022-03-28 13:26:48 +0600

    elco logic from left menu

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/utils/ChildDBConstants.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/utils/ChildDBConstants.java
@@ -66,7 +66,7 @@ public class ChildDBConstants {
 
     }
     public static String elcoFilterWithTableName(){
-        String query = " ((( julianday('now') - julianday(ec_family_member.dob))/365) >" + 10 + ")";
+        String query = " ((( julianday('now') - julianday(ec_family_member.dob))/365) >" + 13 + ")";
         String query2 = " ((( julianday('now') - julianday(ec_family_member.dob))/365) <" + 50 + ")";
         String married = " ec_family_member.marital_status = 'Married' and ec_family_member.gender = 'F'";
         return query+" AND "+query2 +" AND "+married + " AND "+CoreConstants.TABLE_NAME.FAMILY_MEMBER+"."+DBConstants.KEY.BASE_ENTITY_ID+" " +
@@ -74,7 +74,7 @@ public class ChildDBConstants {
                 " and ec_family_member.base_entity_id  NOT IN (select ec_pregnancy_outcome.base_entity_id from ec_pregnancy_outcome where ec_pregnancy_outcome.is_closed = '0' group by ec_pregnancy_outcome.base_entity_id)";
     }
     public static String elcoFilter(){
-        String query = " ((( julianday('now') - julianday(dob))/365) >" + 10 + ")";
+        String query = " ((( julianday('now') - julianday(dob))/365) >" + 13 + ")";
         String query2 = " ((( julianday('now') - julianday(dob))/365) <" + 50 + ")";
         String married = " marital_status = 'Married' and gender = 'F'";
         return query+" AND "+query2 +" AND "+married + " AND "+CoreConstants.TABLE_NAME.FAMILY_MEMBER+"."+DBConstants.KEY.BASE_ENTITY_ID+" " +
