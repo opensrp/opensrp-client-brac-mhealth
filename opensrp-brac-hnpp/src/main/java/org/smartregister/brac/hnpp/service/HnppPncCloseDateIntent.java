9ca4ce3e3bcae4435307c744bc8d1b3278879d2e
mahmud6390 <mahmud6390@gmail.com>
2023-01-10 12:55:49 +0600

    dashboard count mismatch

--- opensrp-brac-hnpp/src/main/java/org/smartregister/brac/hnpp/service/HnppPncCloseDateIntent.java
+++ opensrp-brac-hnpp/src/main/java/org/smartregister/brac/hnpp/service/HnppPncCloseDateIntent.java
@@ -22,13 +22,8 @@ public class HnppPncCloseDateIntent extends CoreChwPncCloseDateIntent {
     protected void onHandleIntent(Intent intent) {
         try {
             SQLiteDatabase database = CoreChwApplication.getInstance().getRepository().getWritableDatabase();
-          /*  String sql1 = "UPDATE ec_anc_register SET is_closed = 1 WHERE ec_anc_register.base_entity_id IN " +
-                    "(select ec_pregnancy_outcome.base_entity_id from ec_pregnancy_outcome where ec_pregnancy_outcome.is_closed = 1) ";*/
-
-            //database.execSQL(sql1);
             String sql = "UPDATE ec_anc_register SET is_closed = 1 WHERE ec_anc_register.base_entity_id IN " +
                     "(select ec_pregnancy_outcome.base_entity_id from ec_pregnancy_outcome where ec_pregnancy_outcome.is_closed = 0) ";
-
             database.execSQL(sql);
         } catch (Exception e) {
             e.printStackTrace();
