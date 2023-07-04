832c9ad786939d1f7c73e8eee33c15810d99b36a
hatanvir <tanvirhasan553@gmail.com>
2023-07-04 10:49:50 +0600

    taskId: UNHEA-91 source:JIRA cause: Feedback. solution: Alert data insert optimization

--- opensrp-brac-hnpp/src/main/java/org/smartregister/unicef/dghs/service/VaccineDueUpdateIntentService.java
+++ opensrp-brac-hnpp/src/main/java/org/smartregister/unicef/dghs/service/VaccineDueUpdateIntentService.java
@@ -34,21 +34,28 @@ public class VaccineDueUpdateIntentService extends IntentService {
        //need to get all due vaccine name,date from alert table group by baseentityid
         String query = "select base_entity_id,dob from ec_child";
         ArrayList<String[]> childs = new ArrayList<>();
-        try (Cursor cursor = HnppApplication.getInstance().getRepository().getReadableDatabase().rawQuery(query, new String[]{})) {
-            if (cursor != null && cursor.getCount() > 0) {
+        Cursor cursor = null;
+        try{
+            cursor = HnppApplication.getInstance().getRepository().getReadableDatabase().rawQuery(query, new String[]{});
+            if(cursor !=null && cursor.getCount() >0){
                 cursor.moveToFirst();
                 while (!cursor.isAfterLast()) {
                     String[] member = new String[2];
 
                     member[0] = cursor.getString(0);
                     member[1] = cursor.getString(1);
-
                     childs.add(member);
                     cursor.moveToNext();
                 }
+
+
             }
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
+
+        }
+        finally {
+            if(cursor!=null) cursor.close();
 
         }
         Log.v("CHILD_FILTER","child list ids>>>>"+childs.size());
