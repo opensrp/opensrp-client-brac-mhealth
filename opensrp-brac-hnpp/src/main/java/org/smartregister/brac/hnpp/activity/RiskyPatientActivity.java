304a716100e48ba7330fd2dd2111c708a4d3eb0a
hatanvir <tanvirhasan553@gmail.com>
2023-10-26 18:09:30 +0600

    taskId:HNPP-1135 source:JIRA cause: CR solution: Back button click feature added

--- opensrp-brac-hnpp/src/main/java/org/smartregister/brac/hnpp/activity/RiskyPatientActivity.java
+++ opensrp-brac-hnpp/src/main/java/org/smartregister/brac/hnpp/activity/RiskyPatientActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import org.smartregister.brac.hnpp.R;
 import org.smartregister.brac.hnpp.adapter.SectionsPagerAdapter;
 
 public class RiskyPatientActivity extends AppCompatActivity {
+    ImageView backIm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +19,17 @@ public class RiskyPatientActivity extends AppCompatActivity {
         setContentView(R.layout.activity_risky_patient);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
+        backIm = findViewById(R.id.back_im);
+
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+
+        backIm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
